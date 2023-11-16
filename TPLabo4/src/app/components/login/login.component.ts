648fb593aef654074cb39dd6928623c28952e9e5
Milagros <68199334+milagrospe@users.noreply.github.com>
2023-11-16 15:58:03 -0300

    Agregado de documentacion y validaciones

--- TPLabo4/src/app/components/login/login.component.ts
+++ TPLabo4/src/app/components/login/login.component.ts
@@ -26,16 +26,36 @@ export class LoginComponent {
 
   })
 
+  /**
+ * @method isValidfield: Checks the validity of a form field using the validation service. 
+ * @param field - The name of the form field to be checked for validity.
+ * @returns A boolean indicating whether the specified form field is valid (true), invalid (false), or the validity is not determined (null).
+ * */
+
 public isValidField(field: string): boolean | null{
 
  return this.validationService.isValidForm(field,this.loginForm);
 }
+
+/**
+ * @method getFieldError: Retrieves the error message associated with a specific form field from the validation service.
+ * @param field - The name of the form field for which to retrieve the error message.
+ * @returns The error message for the specified form field, or null if no error is found.
+ * */
 
 public getFieldError(field: string): string | null{
  
  return this.validationService.getFormError(field,this.loginForm);
 }
 
+
+/**
+ * @method checkAuth: Checks the authentication status of a user and performs appropriate actions based on the result.
+ * This function logs a message to the console, checks the authentication status using the `authService`,
+ * and navigates to the home page if authentication is successful. If authentication fails, it displays
+ * an error message using the SweetAlert2 library.
+ * @throws Error - If an error occurs during the authentication check.
+ */
 
  public async checkAuth(){
     console.log("Hola entre");
@@ -54,6 +74,12 @@ public getFieldError(field: string): string | null{
       alert("No existe el usuario");*/
     }
   } 
+
+/**
+ * @method navigateToRegister: Navigates to the registration page.
+ * This function uses the Angular Router to navigate to the registration page, typically triggered
+ * when a user wants to move from the current page to the registration page.
+ */
 
   public navigateToRegister(){
     this.router.navigate(['/register']);
