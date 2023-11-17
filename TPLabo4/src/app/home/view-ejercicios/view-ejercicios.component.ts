1d00bde97fc239ee52fe62cce0c34d86d7f188aa
NahuelGonzalez7 <nahuelgonzalez07@hotmail.es>
2023-11-17 19:28:29 -0300

    agregar y eliminar favorito funcionando

--- TPLabo4/src/app/home/view-ejercicios/view-ejercicios.component.ts
+++ TPLabo4/src/app/home/view-ejercicios/view-ejercicios.component.ts
@@ -9,9 +9,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { ApiService } from 'src/app/Core/api.service';
 import { ExerciseService } from 'src/app/Core/exercise.service';
-import { Exercise, FavouriteExercise, User,Routine } from 'src/app/Core/models';
+import { Exercise, FavouriteExercise, User } from 'src/app/Core/models';
 import { UserService } from 'src/app/Core/user.service';
-import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -25,7 +24,7 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
 
   public favouriteExercises: FavouriteExercise[] = [];
   public favouriteExercisesIDS: number[] = [];
-  public excercises: Exercise[] = [];
+  public exercises: Exercise[] = [];
   public user: User = new User();
 
   constructor(
@@ -36,62 +35,28 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
   ) { }
 
   ngOnInit(): void {
-    console.log("hola webon");
     this.getLocalStorageUser();
     // this.getUserByID(1);
     // this.getRoutines();
     // this.getRoutineByDay(this.dayOftheweek);
     // this.getLoggedUser();
-    this.exerciseService.getFavouriteExercisesByUser(this.user.id).subscribe(resp => {
-      this.favouriteExercisesIDS = resp.map(x => 
-         x.exerciseID        
-       );
-       this.getExcercises();
-    })
-    // this.getExcercises();
-
-    this.getLocalStorageUser();
-
-    // const storedUser = localStorage.getItem('user');
-    // if (storedUser) {
-    //   const arrayFromLocalStorage = JSON.parse(storedUser);
-    //   console.log(arrayFromLocalStorage);
-    //   const myUser = arrayFromLocalStorage[0];
-    //   this.user = myUser;
-    // } else {
-    //   console.warn('User not found in local storage');
-    // }
-
-
-    // (async () => {
-    //   this.user = await this.getLoggedUser();
-    //   // this.getExcercises();
-    // })();
-
-
-    // console.log(this.activatedRoute.snapshot.data['user']);
-    // this.user = this.user = this.activatedRoute.snapshot.data['user'];
-    // // Check if the user is logged in
-    // if (this.activatedRoute.snapshot.queryParams['isLoggedIn'] === 'true') {
-    //   // Get the user ID from the router state
-    //   const userId = this.activatedRoute.snapshot.data['user'];
-    //   console.log(userId);
-    //   // Get the user object from the database
-    //   this.apiService.getUserById(userId).subscribe((user) => {
-    //     this.user = user;
-    //   });
-    // } else {
-    //   // The user is not logged in
-    //   console.log('se volvio a romper no existe el user');
-    // }
+    this.getFavouriteExercises();
     
     console.log(this.user);
-
+    console.log(this.favouriteExercisesIDS);
+    
     // this.apiService.isExcerciseFavourite(1).subscribe(resp => {console.log(resp)});
     // this.getFavouriteExcercisesByUser(this.user.id);
     // this.apiService.removeFavouriteExcercise(this.routines,1,1).subscribe(resp => {console.log(resp)});
     // this.removeFavouriteExcercise(1,3);
     // this.apiService.addExerciseToFavourite(1,4).subscribe(resp=> {});
+  }
+
+  private getFavouriteExercises() {
+    this.exerciseService.getFavouriteExercisesByUser(this.user.id).subscribe(resp => {
+      this.favouriteExercisesIDS = resp.map(x => x.exerciseID);
+      this.getExcercises();
+    });
   }
 
   private getLocalStorageUser() {
@@ -103,23 +68,7 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
     this.user = myUser;
   }
 
-  ngAfterViewInit(): void {
-    // this.onLikeClick();
-  }
- 
-
-  // getLoggedUser(): User {
-  //   this.userService.currentUser.subscribe((currentUser) => {
-  //     this.apiService
-  //       .getUserLogged(currentUser.email, currentUser.password)
-  //       .subscribe((userLogged) => {
-  //         console.log(userLogged);
-  //         this.user = userLogged;
-  //       });
-  //   });
-  //   console.log("el usuario adentro del metodo pero afuera del service",this.user);
-  //   return this.user;
-  // }
+  ngAfterViewInit(): void { }
 
   getRoutines() {
     this.apiService.getRoutines().subscribe((resp) => {
@@ -142,8 +91,8 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
    */
   getExcercises() {
     this.exerciseService.getExercises().subscribe((resp) => {
-      this.excercises = resp;
-      console.table(this.excercises);
+      this.exercises = resp;
+      console.table(this.exercises);
     });
   }
 
@@ -153,32 +102,35 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
    */
   getExcercisesByDay(dayOftheweek: string) {
     this.exerciseService.getExercisesByType(dayOftheweek).subscribe((resp) => {
-      this.excercises = resp;
+      this.exercises = resp;
       // console.log("estoy aca",this.excercises);
     });
   }
 
-  addExcerciseToFavourite(userID: number | null, exerciseID: number) {
+  addOrDeleteFavouriteExercise(userID: number, exerciseID: number) {
+    console.log(userID);
+    console.log(exerciseID);
     if (userID == null) {
       alert('No hay un usuario registrado');
     } else if (exerciseID == null) {
-      alert('No tenes ejercicios favoritos');
+      alert('No se puede agregar este ejercicio a favorito');
+    } else if(this.isFavourite(exerciseID)){
+      // alert('Se elimino el ejercicio');
+      this.removeFavouriteExcercise(userID,exerciseID);
     } else {
-      this.exerciseService
-        .addExerciseToFavourite(userID, exerciseID)
-        .subscribe((resp) => {
-          console.log(resp);
-        });
+      this.exerciseService.addExerciseToFavourite(userID, exerciseID).subscribe((resp) => {
+        // alert('Se agrego el ejercicio');
+        this.getFavouriteExercises();
+      });
     }
   }
 
   //ToDo PROBAR
   removeFavouriteExcercise(userID: number, excerciseID: number) {
-    this.exerciseService
-      .removeFavouriteExercise(userID, excerciseID)
-      .subscribe((resp) => {
+    this.exerciseService.removeFavouriteExercise(userID, excerciseID).subscribe((resp) => {
         //ToDo agregar sweet alert indicando que se borro bien
         // console.log(resp)
+        this.getFavouriteExercises();
       });
   }
 
@@ -202,8 +154,7 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
 
   isFavourite(exerciseID: number){
     console.log(this.favouriteExercisesIDS.some(x => x == exerciseID));
-    return this.favouriteExercisesIDS.some(x => x == exerciseID);
-
+    return this.favouriteExercisesIDS.some(exercise => exercise == exerciseID);
   }
 
   onLikeClick() {
