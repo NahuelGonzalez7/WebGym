052970b411737a7f4b39ad6ab7ae746c12c7b1b2
AgosC1997 <agostina.cardinali@gmail.com>
2023-11-17 20:22:35 -0300

    Descripcion de los ejercicios en landing page

--- TPLabo4/src/app/home/view-ejercicios/view-ejercicios.component.ts
+++ TPLabo4/src/app/home/view-ejercicios/view-ejercicios.component.ts
@@ -1,19 +1,18 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { ApiService } from 'src/app/Core/api.service';
 import { Excercise, Routine } from 'src/app/Core/models';
-import { HttpClient } from '@angular/common/http';
-
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-view-ejercicios',
   templateUrl: './view-ejercicios.component.html',
-  styleUrls: ['./view-ejercicios.component.css']
+  styleUrls: ['./view-ejercicios.component.css'],
 })
-export class ViewEjerciciosComponent implements OnInit, AfterViewInit{
-
-  public dayOftheweek: string  = "martes";
-  @ViewChild('heartButton') heartButton !:ElementRef<HTMLElement>;
+export class ViewEjerciciosComponent implements OnInit, AfterViewInit {
+  public dayOftheweek: string = 'martes';
+  @ViewChild('heartButton') heartButton!: ElementRef<HTMLElement>;
 
   public routines: Routine[] = [];
   public excercises: Excercise[] = [];
@@ -27,11 +26,11 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit{
   }
 
   ngAfterViewInit(): void {
-    this.onLikeClick();
- }
+    this.onButtonClick();
+  }
 
-  getRoutines(){
-    this.apiService.getRoutines().subscribe(resp => {
+  getRoutines() {
+    this.apiService.getRoutines().subscribe((resp) => {
       // this.routines = resp;
       // console.log(resp.map((data => {data.description})));
       console.log(resp[0]);
@@ -39,7 +38,7 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit{
   }
 
   getRoutineByDay(dayOftheweek: string) {
-    this.apiService.getRoutinesByDay(dayOftheweek).subscribe( resp => {
+    this.apiService.getRoutinesByDay(dayOftheweek).subscribe((resp) => {
       this.routines = resp;
       console.log(resp);
       console.log(this.routines);
@@ -47,33 +46,44 @@ export class ViewEjerciciosComponent implements OnInit, AfterViewInit{
   }
 
   /**
- * Fetches exercises from the API.
- *
- */
+   * Fetches exercises from the API.
+   *
+   */
   getExcercises() {
-    this.apiService.getExcercises().subscribe (resp => {
+    this.apiService.getExcercises().subscribe((resp) => {
       console.table(resp);
       this.excercises = resp;
     });
   }
 
   /**
- * Fetches exercises for a specific day of the week from the API.
- *
- * @param dayOfTheWeek - The day of the week for which exercises are requested.
- */
+   * Fetches exercises for a specific day of the week from the API.
+   *
+   * @param dayOfTheWeek - The day of the week for which exercises are requested.
+   */
   getExcercisesByDay(dayOftheweek: string) {
-    this.apiService.getExcercisesByType(dayOftheweek).subscribe (resp => {
+    this.apiService.getExcercisesByType(dayOftheweek).subscribe((resp) => {
       this.excercises = resp;
-      console.log("estoy aca",this.excercises);
+      console.log('estoy aca', this.excercises);
     });
   }
 
-  onLikeClick() {
-    // TODO: Implement like button functionality here
-    console.log(this.heartButton);
-    if(this.heartButton)
-      this.heartButton.nativeElement.classList.add('clicked');
+  // onLikeClick() {
+  //   // TODO: Implement like button functionality here
+  //   console.log(this.heartButton);
+  //   if(this.heartButton)
+  //     this.heartButton.nativeElement.classList.add('clicked');
+  // }
+
+  // onLikeClick() {
+  //   // Aquí podemos hacer algo cuando se hace clic en el botón
+  //   if (this.heartButton) {
+  //     this.heartButton.nativeElement.classList.add('heart-button-liked');
+  //     NgClass.apply('heart-button-liked');
+  //   }
+  // }
+  active = false;
+  onButtonClick() {
+    this.active = !this.active;
   }
-  
 }
